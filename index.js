34de3a2334aecc98cb19ca5102c9db22831ebb0d
QuaggWasTaken <forrestfaulkner1@gmail.com>
2020-10-04 17:23:55 -0500

    Minimum Viable Product

--- index.js
+++ index.js
@@ -1,16 +1,91 @@
-// array of questions for user
-const questions = [
+//node imports
+'use strict';
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
+
+
+// array of questions for user
+var questions = [{
+        type: "input",
+        name: "title",
+        message: "What is the name of your project repo?"
+    },
+    {
+        type: "input",
+        name: "desc",
+        message: "Enter a description for your project"
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "How do you install your project?"
+    },
+    {
+        type: "input",
+        name: "use",
+        message: "How do you use your project?"
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "What contribution guidelines should you follow?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "How do you test your project?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license does your project use?",
+        choices: ['GNU General Public License', 'MIT License', 'Apache License', 'Ms-PL License', 'Berkeley Software Distribution']
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is yout Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+        validate: function (value) {
+            var pass = value.match(`[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?`);
+            if (pass) {
+              return true;
+            }
+      
+            return 'Please enter a valid email address';
+          },
+    }
 ];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+
+        console.log('README complete! Check out output/README.md to see the output!');
+    })
 }
+var data;
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(JSON.stringify(answers, null, '  '));
+            data = answers;
+            var output = generateMarkdown(data);
+            console.log(output)
+            writeToFile('output/README.md', output);
+        });
 }
 
-// function call to initialize program
+// function call to initialize programinit()
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,58 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![badmath](https://img.shields.io/github/license/${data.github}/${data.title})
+  
+  ## Description 
+  
+  ${data.desc}
+  
+  
+  ## Table of Contents (Optional)
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribting](#contributing)
+  * [Tests](#tests)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Questions](#questions)
+  
+  
+  ## Installation
+  
+  ${data.install}
+  
+  
+  ## Usage 
+  
+  ${data.use}
+  
+  
+  ## Contributing
 
-`;
+  ${data.contribute}
+  
+  
+  ## Tests
+
+  ${data.test}
+  
+  
+  ## Credits
+
+  ${data.credits}
+  
+  
+  ## License
+  
+  ${data.license}
+
+
+  ## Questions
+
+  For additional questions, fing my github page [here](https://github.com/${data.github}/), or email me at <${data.email}>
+  `;
 }
 
 module.exports = generateMarkdown;
